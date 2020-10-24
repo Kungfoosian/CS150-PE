3492a61876a8d0d7c3aa0353b0bfae0c0d17d518
Anthony <anthonyvovo123@gmail.com>
2020-10-24 15:24:15 -0700

    PE08 findFirstOf

---
+++ CS150-PE08-findFirstOf/findFirstOf.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+
+void findFirstOf(const char* s1, const char* s2)
+{
+	int result{ -1 };
+	// Find length of s2
+	const char* s2Ptr = s2;
+	size_t s2Len{ 0 };
+	while (*s2Ptr != '\0')
+	{
+		s2Len++;
+		s2Ptr++;
+	}
+
+	s2Ptr = s2;
+
+	const char* s1Ptr = s1;
+	bool found{ false };
+	int s1Index{ 0 };
+	while (*s1Ptr != '\0')
+	{
+		size_t counter{ 0 };
+		while (counter != s2Len)
+		{
+			if (*(s1Ptr + counter) == *(s2Ptr + counter))
+			{
+				found = true;
+				counter++;
+			}
+			else
+			{
+				found = false;
+				break;
+			}
+
+		}
+
+		if (found)
+		{
+			result = s1Index;
+			break;
+		}
+		else
+		{
+			s1Ptr++;
+			s1Index++;
+		}
+	}
+
+	cout << result << endl;
+}
+
+
+
+int main()
+{
+	findFirstOf("aaaiii", "aia");
+
+	//cout << "HERE" << endl;
+	//cout << findFirstOf("eieioh", "eio") << endl; // Expected: 2 (index of second e)
+	//cout << findFirstOf("aaaiii", "aia") << endl; // Expected: -1 (aia not inside aaaiii)
+	return 0;
+}
